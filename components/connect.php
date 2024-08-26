d179af68ec6e2f78e53af4de9f4572c198cd8099
MiguelVides10 <miguel.vides@catolica.edu.sv>
2024-08-26 11:17:23 -0600

    cambio usuario

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_name = 'mysql:host=localhost;dbname=food_db';
-$user_name = 'root';
-$user_password = '';
+$user_name = 'usuario_remoto';
+$user_password = 'admin';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
