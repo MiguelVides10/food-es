a2ed401b1dccdd1ad822257476481fc264094ea8
MiguelVides10 <miguel.vides@catolica.edu.sv>
2024-08-29 21:50:02 -0600

    cambios en la base

--- food_db.sql
+++ food_db.sql
@@ -27,6 +27,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
+DROP TABLE IF EXISTS `admin`;
+DROP TABLE IF EXISTS `cart`;
+DROP TABLE IF EXISTS `messages`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `admin` (
   `id` int(100) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -87,7 +94,7 @@ CREATE TABLE `orders` (
   `address` varchar(500) NOT NULL,
   `total_products` varchar(1000) NOT NULL,
   `total_price` int(100) NOT NULL,
-  `placed_on` date NOT NULL DEFAULT current_timestamp(),
+  `placed_on` date NULL ,
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -117,7 +124,7 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `number` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `address` varchar(500) NOT NULL
+  `address` varchar(500)  NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
